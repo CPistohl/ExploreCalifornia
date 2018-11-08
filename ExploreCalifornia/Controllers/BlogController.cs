d7e0a4da86479955c84366a7ec8bcdce4e11bc6b
Connor Pistohl <cpistohl@networkhealth.com>
2018-11-08 16:00:27 -0600

    configuring routes

---
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.Controllers
+{
+	public class BlogController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("blog/{year:int}/{month:int}/{key}")]
+		public IActionResult Post(int year, int month, string key)
+		{
+			return new ContentResult { Content = id.ToString() };
+		}
+	}
+}
